99e8457fc0f6898ed902d8186a8f173c155893ed
onthir <bibek09@outlook.com>
2024-11-17 20:10:58 -0600

    updated

--- onthir/settings.py
+++ onthir/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
-import corsheaders
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
