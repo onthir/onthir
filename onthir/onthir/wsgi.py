101bfbe26bb90d36269443ccadd110f05fc044a7
onthir <bibek09@outlook.com>
2024-11-17 21:09:52 -0600

    wsgi

--- onthir/onthir/wsgi.py
+++ onthir/onthir/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onthir.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onthir.onthir.settings')
 
 application = get_wsgi_application()
